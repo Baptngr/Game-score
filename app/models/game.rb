17978d32e4643b12d084ae19527764eefb37b844
Baptngr <baptiste.nogueras@mmibordeaux.com>
2025-02-21 17:59:08 +0100

    TO S

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,7 @@ class Game < ApplicationRecord
   belongs_to :team_away, class_name: 'Team'
   belongs_to :team_home, class_name: 'Team'
   belongs_to :day
+   def to_s
+    score_home + score_away
+   end
 end
